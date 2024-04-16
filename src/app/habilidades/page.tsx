259e03c489d271e8ebcbfcc8fc5cfbf53a518c93
Morales <125131236+MoralesDaxz@users.noreply.github.com>
2024-04-16 22:26:17 +0200

    🚑 Problems navBar 3

--- src/app/habilidades/page.tsx
+++ src/app/habilidades/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='w-full min-h-screen bg-slate-800'>
-      <p>NavBar Problems 1</p>
+      <p>NavBar Problems 3</p>
     </div>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,38 +14,30 @@ const NavBar = () => {
 
     const styleMD = 'z-10 w-full fixed top-0 py-2 flex justify-center items-start sm:gap-10 md:gap-20'
 
-    const [controlWidth, setControlWidth] = useState('SM')
+    const [controlWidth, setControlWidth] = useState('')
     const [closeModal, setCloseModal] = useState(true)
     const [bgNavBar, setBgNavBar] = useState(false)
 
-    const checkWidthDisplay = () => {
-        return window.innerWidth < 640 ? setControlWidth('SM') : setControlWidth('MD')
-    }
-    const checkScrollDisplay = () => {
-        return window.scrollY > 40 ? setBgNavBar(true) : setBgNavBar(false)
-    }
-    useEffect(() => {
-        checkScrollDisplay()
-        checkWidthDisplay()
 
-        window.addEventListener('resize', () => checkWidthDisplay())
-        window.addEventListener('scroll', () => checkScrollDisplay())
-        return (
-            window.removeEventListener('resize', () => checkWidthDisplay()),
-            window.removeEventListener('scroll', () => checkScrollDisplay())
-        )
+    useEffect(() => {
+        window.addEventListener('resize', () => window.innerWidth < 640 ? setControlWidth('SM') : setControlWidth('MD'))
+        window.addEventListener('scroll', () => window.scrollY > 40 ? setBgNavBar(true) : setBgNavBar(false))
+        return ()=>{
+            window.removeEventListener('resize', () => window.innerWidth < 640 ? setControlWidth('SM') : setControlWidth('MD')),
+            window.removeEventListener('scroll', () => window.scrollY > 40 ? setBgNavBar(true) : setBgNavBar(false))
+        }
     })
 
     return (
         < >
-            {window.innerWidth > 640 ?
+            {controlWidth === 'MD' ?
                 <div className={bgNavBar ? `z-20 bg-[#02171fad] py-2 backdrop-blur-sm ${styleMD}` : styleMD} >
                     <Link className='absolute top-[25%] left-8 z-20' href={'/'}>
                         <Image priority src={'https://i.ibb.co/wwqJ9qC/favicon-removebg-preview.png'} width={40} height={40} alt='logo' title='logo' />
                     </Link>
                     {route.map((item, index) => { return <Link className=' text-base sm:text-lg lg:text-xl font-medium transition-all duration-700 text-[white] opacity-80 hover:opacity-100 hover:bg-bondiBlue-950 z-10 rounded-md border-[3px] border-bondiBlue-900 hover:border-bondiBlue-500  hover:shadow-md hover:shadow-bondiBlue-800 px-5 py-3' key={index} href={item.link}>{item.title}</Link> })}
-                </div> :
-                    <Aside closeModal={closeModal} controlWidth={controlWidth} setCloseModal={setCloseModal} />}
+                </div> :controlWidth === 'SM' ?
+                <Aside closeModal={closeModal} controlWidth={controlWidth} setCloseModal={setCloseModal} />:''}
         </>
     )
 }
