aab4c9c0cf5400b82d481a98edd39cf1236a1cbf
Morales <125131236+MoralesDaxz@users.noreply.github.com>
2024-04-18 08:31:33 +0200

    Actualizar layout.tsx
    Favocon asignado al content en meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <meta property="og:description" content="Soy Alfredo Morales, presentación de mi portafolio web, proyectos y entornos prácticos desarrollados." />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="es_ES" />
-        <meta property="og:image" content="https://i.ibb.co/wwqJ9qC/favicon-removebg-preview.png" />
+        <meta property="og:image" content="https://i.ibb.co/54ktrkM/portfolio-Icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
