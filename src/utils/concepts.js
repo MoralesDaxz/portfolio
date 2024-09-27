b9fb84d00cb88a3c8405f21987068f38caa06388
moralesdaxz <alfredojose.92@gmail.com>
2024-09-27 17:23:26 +0200

    Update inmage and links

--- src/utils/concepts.js
+++ src/utils/concepts.js
@@ -9,15 +9,15 @@ const aboutMe = [
 const cafeAlturaData = {
   title: "Cafe con Aroma",
   prf: "Página creada implementando React, API, Tailwind, CSS, Librerías, a partir de boceto en Figma a peticion de cliente (empresa comercializadora de café). Proyecto presentado como evaluación de conocimientos adquiridos en formación práctica de Gamma Tech School.",
-  img: "https://i.ibb.co/FBP4qC6/cafe.png",
+  img: "https://i.ibb.co/VJTgNjV/cafeconaroma.png",
   alt: "cafe_aroma",
-  url: "https://cafeconaroma.netlify.app/",
+  url: "https://cafeconaroma.vercel.app/",
   skills: ["React", "Api", "TailwindCSS", "Library"],
 };
 const globalLogisticData = {
   title: "Global Logistic",
   prf: "Página basada en empresa, servicio de transporte y paquetería, presentada como proyecto final de curso Desarrollo Web, implementando React, Tailwind, CSS, UseForm, ReactToPrint, ReactIcons, entre otras librerías.",
-  img: "https://i.ibb.co/n7GNGTB/global.png",
+  img: "https://i.ibb.co/n7GNGTB/globallogistic.png",
   alt: "global_logistic",
   url: "https://global-logistic-final.vercel.app/",
   skills: ["React", "TailwindCSS", "Library"],
