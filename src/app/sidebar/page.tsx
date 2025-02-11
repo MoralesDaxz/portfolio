2c2560d29dc9455cf640004e218942fd72b7ffc8
Morales <125131236+MoralesDaxz@users.noreply.github.com>
2025-02-11 21:34:13 +0100

    🐛 Fix a bug

--- src/app/sidebar/page.tsx
+++ src/app/sidebar/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <>
+    <> 
       <SideBar>
         <div className="w-full">
           <h1 className="text-center font-extrabold mt-10">Contenido</h1>
